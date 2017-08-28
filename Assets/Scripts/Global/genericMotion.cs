1a7b4f00be0dc5b50a0f4caee1bab0152d8d6767
Alexander Fatemi-Badi <alexfatemi@gmail.com>
2017-08-28 19:29:05 -0400

    Updated Unity and other stuff

---
+++ Assets/Scripts/Global/genericMotion.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class genericMotion : MonoBehaviour {
+	public bool active = true;
+	public enum type {
+		translate,
+		rotate,
+		scale,
+		textureOffset,
+	}
+	public type motionType;
+	public float speed = 1f;
+	public bool xAxis = false;
+	public bool yAxis = false;
+	public bool zAxis = false;
+	private Renderer mat;
+	// Use this for initialization
+	void Start () {
+		mat = GetComponent<Renderer> ();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		if (active) {
+			if (motionType == type.translate) {
+				Vector3 currentPos = transform.localPosition;
+				if (xAxis)
+					currentPos = currentPos + (transform.right * speed);
+				if (yAxis)
+					currentPos = currentPos + (transform.up * speed);
+				if (zAxis)
+					currentPos = currentPos + (transform.forward * speed);
+				transform.localPosition = currentPos;
+			} else if (motionType == type.scale) {
+				Vector3 currentScale = transform.localScale;
+				if (xAxis)
+					currentScale.x += speed;
+				if (yAxis)
+					currentScale.y += speed;
+				if (zAxis)
+					currentScale.z += speed;
+				transform.localScale = currentScale;
+			} else if (motionType == type.rotate) {
+				Vector3 currentRot = transform.localEulerAngles;
+				if (xAxis)
+					currentRot.x += speed;
+				if (yAxis)
+					currentRot.y += speed;
+				if (zAxis)
+					currentRot.z += speed;
+				transform.localEulerAngles = currentRot;
+			} else if (motionType == type.textureOffset) {
+				Vector2 currentUV = mat.material.mainTextureOffset;
+				if (xAxis)
+					currentUV.x += speed;
+				if (yAxis)
+					currentUV.y += speed;
+				mat.material.mainTextureOffset = currentUV;
+			}
+		}
+	}
+}
---
+++ Assets/Scripts/Level_Mine/ccGun.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ccGun : enemy {
+	private Animator anim;
+	private Transform target;
+	public GameObject bullet;
+	private bool active;
+	private bool attacking;
+	private bool follow;
+	private Vector3 lookTarget;
+	private Transform shotSpawn;
+
+	// Use this for initialization
+	void Start () {
+		anim = GetComponentInChildren<Animator> ();
+		target = player.mainPlayer.transform;
+		shotSpawn = transform.GetChild (1);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (active && !attacking)
+			StartCoroutine (attack ());
+		if (follow) {
+			lookTarget = target.position;
+			lookTarget.y = transform.position.y;
+			transform.LookAt (lookTarget);
+		}
+	}
+	void OnTriggerEnter(Collider other){
+		if (other.transform == target)
+			active = true;
+	}
+	void OnTriggerExit(Collider other){
+		if (other.transform == target)
+			active = false;
+	}
+
+	IEnumerator attack () {
+		attacking = true;
+		follow = true;
+		anim.SetTrigger ("attack");
+		yield return new WaitForSeconds (0.4f);
+		Vector3 targetPos1 = target.position;
+		yield return null;
+		Vector3 targetPos2 = target.position;
+		follow = false;
+		yield return null;
+		Vector3 shotTarget = targetPos2 - targetPos1;
+		shotTarget = targetPos2 + (shotTarget * 1);
+		shotTarget.y = transform.position.y;
+		transform.LookAt (shotTarget);
+		Instantiate (bullet, shotSpawn.position, transform.rotation);
+		yield return new WaitForSeconds(1.2f);
+		attacking = false;
+	}
+}
